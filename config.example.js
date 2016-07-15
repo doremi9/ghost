ebe9f063a511861f620821a30feff985d2b5b450
david-dimas333 <david.dimas333@gmail.com>
2016-07-15 16:44:35 +0200

    Update url

--- config.example.js
+++ config.example.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://my-ghost-blog.com',
+        url: 'http://ghost3-david-ghost.apps.thedigitalgarage.io/',
         mail: {},
         database: {
             client: 'sqlite3',
